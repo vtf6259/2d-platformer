03f8ed2d90f478ca2778a42cd9919dfcec2c01d7
vtf6259 <125233907+vtf6259@users.noreply.github.com>
2026-04-19 19:03:55 -0700

    Move to c++

--- src/level.cpp
+++ src/level.cpp
@@ -9,8 +9,8 @@
 void loadLevel(Player *player) {
     //currentLevel.player = CreatePlayer(0, 0, 50, 50, PLAYER_TEXTURE, 0, 700, 0, 500);
     currentLevel.player = player;
-    currentLevel.levelData.platformData = malloc(sizeof(platformDynamic));
-    currentLevel.levelData.platformData->count = 0;
+    // currentLevel.levelData.platformData = malloc(sizeof(platformDynamic));
+    // currentLevel.levelData.platformData->count = 0;
 
 }
 void addPlatform(int arrPos) {
